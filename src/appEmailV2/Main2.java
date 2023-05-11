89c0e3e82e2a67acabfbd9fb21065c94f902bb70
Joabe Lopes <104812737+joabeslopes@users.noreply.github.com>
2023-05-11 16:40:30 -0300

    Update Main2.java
    Mudança no nome dos painéis, na url do outlook, e na lógica da cópia do email para a área de transferência.

--- src/appEmailV2/Main2.java
+++ src/appEmailV2/Main2.java
@@ -37,7 +37,7 @@ public class Main2 extends JFrame {
 
 	private JPanel Principal;
 	private JTextField textName;
-	private JTable infoProf;
+	private JTable tabelaInfoProfs;
 	private String email;
 	private String selecaoEmail;
 	private int selecaoLinha;
@@ -59,41 +59,40 @@ public class Main2 extends JFrame {
 			}
 		});
 	}
-	
-	
+
 	
 	public Main2() {
 		
 		setTitle("Pesquisa de email");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(400, 300, 400, 170);
+		setBounds(500, 300, 400, 170);
 		Principal = new JPanel();
 		Principal.setBackground(new Color(26, 95, 180));
 		Principal.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(Principal);
 		
 		
-		JPanel panel3 = new JPanel();
-		panel3.setBackground(new Color(26, 95, 180));
-		panel3.setVisible(false);
+		JPanel painelEmail = new JPanel();
+		painelEmail.setBackground(new Color(26, 95, 180));
+		painelEmail.setVisible(false);
 		
 		
-		JPanel panel4 = new JPanel();
-		panel4.setVisible(false);
+		JPanel outrasInfos = new JPanel();
+		outrasInfos.setVisible(false);
 		
 		
-		JPanel panel2 = new JPanel();
-		FlowLayout flowLayout = (FlowLayout) panel2.getLayout();
-		flowLayout.setHgap(50);
-		panel2.setBackground(new Color(26, 95, 180));
+		JPanel painelPesquisa = new JPanel();
+		FlowLayout fl_painelPesquisa = (FlowLayout) painelPesquisa.getLayout();
+		fl_painelPesquisa.setHgap(50);
+		painelPesquisa.setBackground(new Color(26, 95, 180));
 		
 		
-		JScrollPane scrollPane5 = new JScrollPane();
-		scrollPane5.setVisible(false);
+		JScrollPane infoProfs = new JScrollPane();
+		infoProfs.setVisible(false);
 		
 		
-		JPanel panel1 = new JPanel();
-		panel1.setBackground(new Color(26, 95, 180));
+		JPanel painelNome = new JPanel();
+		painelNome.setBackground(new Color(26, 95, 180));
 		
 		
 		GroupLayout gl_Principal = new GroupLayout(Principal);
@@ -103,39 +102,39 @@ public class Main2 extends JFrame {
 					.addGroup(gl_Principal.createParallelGroup(Alignment.TRAILING)
 						.addGroup(Alignment.LEADING, gl_Principal.createSequentialGroup()
 							.addContainerGap()
-							.addComponent(panel1, GroupLayout.DEFAULT_SIZE, 401, Short.MAX_VALUE))
+							.addComponent(painelNome, GroupLayout.DEFAULT_SIZE, 401, Short.MAX_VALUE))
 						.addGroup(gl_Principal.createSequentialGroup()
 							.addContainerGap()
-							.addComponent(scrollPane5, GroupLayout.DEFAULT_SIZE, 401, Short.MAX_VALUE))
+							.addComponent(infoProfs, GroupLayout.DEFAULT_SIZE, 401, Short.MAX_VALUE))
 						.addGroup(gl_Principal.createSequentialGroup()
 							.addContainerGap()
-							.addComponent(panel4, GroupLayout.DEFAULT_SIZE, 401, Short.MAX_VALUE))
+							.addComponent(outrasInfos, GroupLayout.DEFAULT_SIZE, 401, Short.MAX_VALUE))
 						.addGroup(gl_Principal.createSequentialGroup()
 							.addContainerGap()
-							.addComponent(panel3, GroupLayout.DEFAULT_SIZE, 401, Short.MAX_VALUE))
+							.addComponent(painelEmail, GroupLayout.DEFAULT_SIZE, 401, Short.MAX_VALUE))
 						.addGroup(gl_Principal.createSequentialGroup()
 							.addContainerGap()
-							.addComponent(panel2, GroupLayout.DEFAULT_SIZE, 401, Short.MAX_VALUE)))
+							.addComponent(painelPesquisa, GroupLayout.DEFAULT_SIZE, 401, Short.MAX_VALUE)))
 					.addContainerGap())
 		);
 		gl_Principal.setVerticalGroup(
 			gl_Principal.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_Principal.createSequentialGroup()
 					.addContainerGap()
-					.addComponent(panel1, GroupLayout.PREFERRED_SIZE, 67, GroupLayout.PREFERRED_SIZE)
+					.addComponent(painelNome, GroupLayout.PREFERRED_SIZE, 67, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(panel2, GroupLayout.PREFERRED_SIZE, 33, GroupLayout.PREFERRED_SIZE)
+					.addComponent(painelPesquisa, GroupLayout.PREFERRED_SIZE, 33, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(panel3, GroupLayout.PREFERRED_SIZE, 34, GroupLayout.PREFERRED_SIZE)
+					.addComponent(painelEmail, GroupLayout.PREFERRED_SIZE, 34, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(panel4, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addComponent(outrasInfos, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(scrollPane5, GroupLayout.DEFAULT_SIZE, 110, Short.MAX_VALUE))
+					.addComponent(infoProfs, GroupLayout.DEFAULT_SIZE, 110, Short.MAX_VALUE))
 		);
 		
 		
-		infoProf = new JTable();
-		infoProf.setModel(new DefaultTableModel(
+		tabelaInfoProfs = new JTable();
+		tabelaInfoProfs.setModel(new DefaultTableModel(
 			new Object[][] {
 			},
 			new String[] {
@@ -149,20 +148,18 @@ public class Main2 extends JFrame {
 				return columnTypes[columnIndex];
 			}
 		});
-		infoProf.getColumnModel().getColumn(0).setPreferredWidth(20);
-		infoProf.getColumnModel().getColumn(0).setMinWidth(20);
-		infoProf.getColumnModel().getColumn(1).setPreferredWidth(90);
-		infoProf.getColumnModel().getColumn(1).setMinWidth(20);
-		infoProf.getColumnModel().getColumn(2).setPreferredWidth(200);
-		infoProf.getColumnModel().getColumn(2).setMinWidth(20);
-		infoProf.getColumnModel().getColumn(3).setPreferredWidth(60);
-		infoProf.getColumnModel().getColumn(3).setMinWidth(20);
-		infoProf.getColumnModel().getColumn(4).setPreferredWidth(90);
-		infoProf.getColumnModel().getColumn(4).setMinWidth(20);
-		infoProf.getColumnModel().getColumn(5).setPreferredWidth(0);
-		infoProf.getColumnModel().getColumn(5).setMinWidth(0);
-		infoProf.getColumnModel().getColumn(5).setMaxWidth(0);
-		scrollPane5.setViewportView(infoProf);
+		tabelaInfoProfs.getColumnModel().getColumn(0).setPreferredWidth(20);
+		tabelaInfoProfs.getColumnModel().getColumn(0).setMinWidth(20);
+		tabelaInfoProfs.getColumnModel().getColumn(1).setPreferredWidth(90);
+		tabelaInfoProfs.getColumnModel().getColumn(1).setMinWidth(20);
+		tabelaInfoProfs.getColumnModel().getColumn(2).setPreferredWidth(130);
+		tabelaInfoProfs.getColumnModel().getColumn(2).setMinWidth(20);
+		tabelaInfoProfs.getColumnModel().getColumn(3).setMinWidth(20);
+		tabelaInfoProfs.getColumnModel().getColumn(4).setPreferredWidth(90);
+		tabelaInfoProfs.getColumnModel().getColumn(4).setMinWidth(20);
+		tabelaInfoProfs.getColumnModel().getColumn(5).setMinWidth(0);
+		tabelaInfoProfs.getColumnModel().getColumn(5).setMaxWidth(0);
+		infoProfs.setViewportView(tabelaInfoProfs);
 		
 		
 		
@@ -178,28 +175,28 @@ public class Main2 extends JFrame {
 		
 		
 		
-		GroupLayout gl_panel1 = new GroupLayout(panel1);
-		gl_panel1.setHorizontalGroup(
-			gl_panel1.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel1.createSequentialGroup()
+		GroupLayout gl_painelNome = new GroupLayout(painelNome);
+		gl_painelNome.setHorizontalGroup(
+			gl_painelNome.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_painelNome.createSequentialGroup()
 					.addGap(75)
 					.addComponent(lblNewLabel, GroupLayout.DEFAULT_SIZE, 323, Short.MAX_VALUE)
 					.addGap(61))
-				.addGroup(Alignment.TRAILING, gl_panel1.createSequentialGroup()
+				.addGroup(Alignment.TRAILING, gl_painelNome.createSequentialGroup()
 					.addGap(86)
 					.addComponent(textName, GroupLayout.DEFAULT_SIZE, 286, Short.MAX_VALUE)
 					.addGap(87))
 		);
-		gl_panel1.setVerticalGroup(
-			gl_panel1.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel1.createSequentialGroup()
+		gl_painelNome.setVerticalGroup(
+			gl_painelNome.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_painelNome.createSequentialGroup()
 					.addGap(7)
 					.addComponent(lblNewLabel)
 					.addGap(18)
 					.addComponent(textName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 					.addGap(8))
 		);
-		panel1.setLayout(gl_panel1);
+		painelNome.setLayout(gl_painelNome);
 		
 		
 		JLabel lblNewLabel_1 = new JLabel("Email:");
@@ -211,45 +208,45 @@ public class Main2 extends JFrame {
 		textEmail.setText(null);
 		
 		
-		GroupLayout gl_panel3 = new GroupLayout(panel3);
-		gl_panel3.setHorizontalGroup(
-			gl_panel3.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel3.createSequentialGroup()
+		GroupLayout gl_painelEmail = new GroupLayout(painelEmail);
+		gl_painelEmail.setHorizontalGroup(
+			gl_painelEmail.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_painelEmail.createSequentialGroup()
 					.addGap(24)
 					.addComponent(lblNewLabel_1)
 					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addComponent(textEmail, GroupLayout.DEFAULT_SIZE, 317, Short.MAX_VALUE))
 		);
-		gl_panel3.setVerticalGroup(
-			gl_panel3.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel3.createSequentialGroup()
+		gl_painelEmail.setVerticalGroup(
+			gl_painelEmail.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_painelEmail.createSequentialGroup()
 					.addContainerGap()
-					.addGroup(gl_panel3.createParallelGroup(Alignment.BASELINE)
+					.addGroup(gl_painelEmail.createParallelGroup(Alignment.BASELINE)
 						.addComponent(textEmail, GroupLayout.PREFERRED_SIZE, 17, GroupLayout.PREFERRED_SIZE)
 						.addComponent(lblNewLabel_1))
 					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 		);
-		panel3.setLayout(gl_panel3);
+		painelEmail.setLayout(gl_painelEmail);
 		
 		
-		panel4.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		outrasInfos.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
 		
 		JLabel lblOutrasInformaes = new JLabel("Outras informações");
 		lblOutrasInformaes.setHorizontalAlignment(SwingConstants.CENTER);
 		lblOutrasInformaes.setFont(new Font("Dialog", Font.BOLD, 11));
-		panel4.add(lblOutrasInformaes);
+		outrasInfos.add(lblOutrasInformaes);
 		Principal.setLayout(gl_Principal);
 		
 		
 		
 		JButton btnSearchName = new JButton("Pesquisar nome");
-		panel2.add(btnSearchName);
+		painelPesquisa.add(btnSearchName);
 		
 		
 		
 		JButton btnEnviarEmail = new JButton("Enviar email");
-		panel2.add(btnEnviarEmail);
+		painelPesquisa.add(btnEnviarEmail);
 		btnEnviarEmail.setVisible(false);
 		
 
@@ -262,7 +259,7 @@ public class Main2 extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				
 			//Limpa a tela
-				DefaultTableModel df = (DefaultTableModel) infoProf.getModel();
+				DefaultTableModel df = (DefaultTableModel) tabelaInfoProfs.getModel();
 				df.setRowCount(0);
 				textEmail.setText(null);
 				
@@ -276,11 +273,12 @@ public class Main2 extends JFrame {
 					
 					
 					//Altera a tela do usuário
+					setBounds(500, 300, 500, 370);
 					btnEnviarEmail.setVisible(true);
-					panel3.setVisible(true);
-					panel4.setVisible(true);
-					scrollPane5.setVisible(true);
-					setBounds(400, 300, 500, 370);
+					painelEmail.setVisible(true);
+					outrasInfos.setVisible(true);
+					infoProfs.setVisible(true);
+					
 					
 					//mostra o primeiro email encontrado
 					textEmail.setText(email);
@@ -297,10 +295,10 @@ public class Main2 extends JFrame {
 				}else {
 					//Altera a tela do usuário
 					btnEnviarEmail.setVisible(false);
-					panel3.setVisible(false);
-					panel4.setVisible(false);
-					scrollPane5.setVisible(false);
-					setBounds(400, 300, 400, 170);
+					painelEmail.setVisible(false);
+					outrasInfos.setVisible(false);
+					infoProfs.setVisible(false);
+					setBounds(500, 300, 400, 170);
 					
 					//Mensagem de erro
 					JOptionPane.showMessageDialog(btnSearchName, "Nome não encontrado, tente novamente");
@@ -322,7 +320,7 @@ public class Main2 extends JFrame {
 				    	
 				    	
 						// Copia o email selecionado da tabela para a área de transferência
-
+				    	selecaoEmail = textEmail.getText();
 				    	StringSelection selection = new StringSelection(selecaoEmail);
 				    	
 						if (!selection.equals(null)) {
@@ -333,8 +331,8 @@ public class Main2 extends JFrame {
 							JOptionPane.showMessageDialog(btnSearchName, "Email copiado para a área de transferência !");
 						}
 						
-					//Desktop.getDesktop().browse(new URI("https://serverjoabe.xyz:8090/snappymail/"));
-				    	Desktop.getDesktop().browse(new URI("https://outlook.live.com/owa/?nlp=1"));
+						//Desktop.getDesktop().browse(new URI("https://serverjoabe.xyz:8090/snappymail/"));
+						Desktop.getDesktop().browse(new URI("https://outlook.office.com"));
 					} catch (Exception e1) {
 						e1.printStackTrace();
 					}
@@ -347,15 +345,12 @@ public class Main2 extends JFrame {
 		
 		
 		// Reconhece a linha da tabela que o usuário clica, e coloca o email do professor selecionado na tela
-		infoProf.addMouseListener(new MouseAdapter() {
+		tabelaInfoProfs.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
-				if(!textEmail.getText().equals(null)) {
-					selecaoLinha = infoProf.getSelectedRow();
+					selecaoLinha = tabelaInfoProfs.getSelectedRow();
 					if (selecaoLinha !=-1) {
-					selecaoEmail = infoProf.getValueAt(selecaoLinha, 5).toString();
-					textEmail.setText(selecaoEmail);
+					textEmail.setText(tabelaInfoProfs.getValueAt(selecaoLinha, 5).toString());
 					}
-				}
 			}
 		});
 
@@ -370,7 +365,7 @@ public class Main2 extends JFrame {
 					
 					
 					//Limpa a tela
-						DefaultTableModel df = (DefaultTableModel) infoProf.getModel();
+						DefaultTableModel df = (DefaultTableModel) tabelaInfoProfs.getModel();
 						df.setRowCount(0);
 						textEmail.setText(null);
 						
@@ -384,11 +379,12 @@ public class Main2 extends JFrame {
 							
 							
 							//Altera a tela do usuário
+							setBounds(500, 300, 500, 370);
 							btnEnviarEmail.setVisible(true);
-							panel3.setVisible(true);
-							panel4.setVisible(true);
-							scrollPane5.setVisible(true);
-							setBounds(400, 300, 500, 370);
+							painelEmail.setVisible(true);
+							outrasInfos.setVisible(true);
+							infoProfs.setVisible(true);
+							
 							
 							//mostra o primeiro email encontrado
 							textEmail.setText(email);
@@ -405,10 +401,10 @@ public class Main2 extends JFrame {
 						}else {
 							//Altera a tela do usuário
 							btnEnviarEmail.setVisible(false);
-							panel3.setVisible(false);
-							panel4.setVisible(false);
-							scrollPane5.setVisible(false);
-							setBounds(400, 300, 400, 170);
+							painelEmail.setVisible(false);
+							outrasInfos.setVisible(false);
+							infoProfs.setVisible(false);
+							setBounds(500, 300, 400, 170);
 							
 							//Mensagem de erro
 							JOptionPane.showMessageDialog(btnSearchName, "Nome não encontrado, tente novamente");
